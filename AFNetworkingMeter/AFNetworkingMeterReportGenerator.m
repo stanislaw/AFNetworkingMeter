b1e7fb65a57d57b3f7a53a29c77f8dd00023a7d4
stanislaw <s.pankevich@gmail.com>
2013-09-11 22:37:16 +0300

    Semi-commit: -formattedReportForData: #2

--- AFNetworkingMeter/AFNetworkingMeterReportGenerator.m
+++ AFNetworkingMeter/AFNetworkingMeterReportGenerator.m
@@ -28,13 +28,6 @@ NSString *NSStringFromCharacterAndLength(NSString *character, NSUInteger length)
 
     NSString *stringWithLengthEqualToReportWidthAndFilledWithSpaces = NSStringFromCharacterAndLength(@" ", REPORT_WIDTH);
 
-#pragma mark Header
-
-    NSString *headerTop = NSStringFromCharacterAndLength(@"=", REPORT_WIDTH);
-    NSString *headerTitle = @"   AFNetworkingMeter  -  formatted report   ";
-    NSString *headerBottom = NSStringFromCharacterAndLength(@"-", REPORT_WIDTH);
-    NSString *headerString = [@[headerTop, headerTitle, headerBottom] componentsJoinedByString:@"\n"];
-
 #pragma mark Summary
 
     NSString *requestsKey = @"Requests:";
@@ -44,6 +37,7 @@ NSString *NSStringFromCharacterAndLength(NSString *character, NSUInteger length)
     [requestsString replaceCharactersInRange:NSMakeRange(0, requestsKey.length) withString:requestsKey];
     [requestsString replaceCharactersInRange:NSMakeRange(REPORT_WIDTH - requestsValue.length, requestsValue.length) withString:requestsValue];
 
+
     NSString *responsesKey = @"Responses:";
     NSString *responsesValue = [[data valueForKey:AFNetworkingMeterDataResponses] stringValue];
 
@@ -51,12 +45,21 @@ NSString *NSStringFromCharacterAndLength(NSString *character, NSUInteger length)
     [responsesString replaceCharactersInRange:NSMakeRange(0, responsesKey.length) withString:responsesKey];
     [responsesString replaceCharactersInRange:NSMakeRange(REPORT_WIDTH - responsesValue.length, responsesValue.length) withString:responsesValue];
 
-    NSNumber *bytesSent = [data valueForKey:AFNetworkingMeterDataBytesSent];
-    NSString *bytesSentString = [NSString stringWithFormat:@"Sent (bytes): %@", bytesSent];
+
+    NSString *bytesSentKey = @"Sent (bytes):";
+    NSString *bytesSentValue = [[data valueForKey:AFNetworkingMeterDataBytesSent] stringValue];
+
+    NSMutableString *bytesSentString = [stringWithLengthEqualToReportWidthAndFilledWithSpaces mutableCopy];
+    [bytesSentString replaceCharactersInRange:NSMakeRange(0, bytesSentKey.length) withString:bytesSentKey];
+    [bytesSentString replaceCharactersInRange:NSMakeRange(REPORT_WIDTH - bytesSentValue.length, bytesSentValue.length) withString:bytesSentValue];
 
 
-    NSNumber *bytesReceived = [data valueForKey:AFNetworkingMeterDataBytesReceived];
-    NSString *bytesReceivedString = [NSString stringWithFormat:@"Received (bytes): %@", bytesReceived];
+    NSString *bytesReceivedKey = @"Received (bytes):";
+    NSString *bytesReceivedValue = [[data valueForKey:AFNetworkingMeterDataBytesReceived] stringValue];
+
+    NSMutableString *bytesReceivedString = [stringWithLengthEqualToReportWidthAndFilledWithSpaces mutableCopy];
+    [bytesReceivedString replaceCharactersInRange:NSMakeRange(0, bytesReceivedKey.length) withString:bytesReceivedKey];
+    [bytesReceivedString replaceCharactersInRange:NSMakeRange(REPORT_WIDTH - bytesReceivedValue.length, bytesReceivedValue.length) withString:bytesReceivedValue];
 
 #pragma mark Elapsed time
 
@@ -66,19 +69,38 @@ NSString *NSStringFromCharacterAndLength(NSString *character, NSUInteger length)
     numberFormatter.maximumFractionDigits = 7;
     numberFormatter.minimumIntegerDigits = 1;
 
-    NSNumber *minimalElapsedTime = [data valueForKey:AFNetworkingMeterDataMinimalElapsedTimeForRequest];
-    NSString *minimalElapsedTimeString = [NSString stringWithFormat:@"Minimal elapsed time for request (seconds): %@", [numberFormatter stringFromNumber:minimalElapsedTime]];
 
-    NSNumber *maximalElapsedTime = [data valueForKey:AFNetworkingMeterDataMaximalElapsedTimeForRequest];
-    NSString *maximalElapsedTimeString = [NSString stringWithFormat:@"Maximal elapsed time for request (seconds): %@", [numberFormatter stringFromNumber:maximalElapsedTime]];
+    NSString *minimalElapsedTimeKey = @"Min (seconds):";
+    NSString *minimalElapsedTimeValue = [numberFormatter stringFromNumber:[data valueForKey:AFNetworkingMeterDataMinimalElapsedTimeForRequest]];
+
+    NSMutableString *minimalElapsedTimeString = [stringWithLengthEqualToReportWidthAndFilledWithSpaces mutableCopy];
+    [minimalElapsedTimeString replaceCharactersInRange:NSMakeRange(0, minimalElapsedTimeKey.length) withString:minimalElapsedTimeKey];
+    [minimalElapsedTimeString replaceCharactersInRange:NSMakeRange(REPORT_WIDTH - minimalElapsedTimeValue.length, minimalElapsedTimeValue.length) withString:minimalElapsedTimeValue];
+
+
+    NSString *maximalElapsedTimeKey = @"Min (seconds):";
+    NSString *maximalElapsedTimeValue = [numberFormatter stringFromNumber:[data valueForKey:AFNetworkingMeterDataMaximalElapsedTimeForRequest]];
+
+    NSMutableString *maximalElapsedTimeString = [stringWithLengthEqualToReportWidthAndFilledWithSpaces mutableCopy];
+    [maximalElapsedTimeString replaceCharactersInRange:NSMakeRange(0, maximalElapsedTimeKey.length) withString:maximalElapsedTimeKey];
+    [maximalElapsedTimeString replaceCharactersInRange:NSMakeRange(REPORT_WIDTH - maximalElapsedTimeValue.length, maximalElapsedTimeValue.length) withString:maximalElapsedTimeValue];
 
 #pragma mark Image requests
 
-    NSNumber *imageRequests = [data valueForKey:AFNetworkingMeterDataImageRequests];
-    NSString *imageRequestsString = [NSString stringWithFormat:@"Image requests: %@", imageRequests];
+    NSString *imageRequestsKey = @"Requests:";
+    NSString *imageRequestsValue = [[data valueForKey:AFNetworkingMeterDataImageRequests] stringValue];
 
-    NSNumber *imageResponses = [data valueForKey:AFNetworkingMeterDataImageResponses];
-    NSString *imageResponsesString = [NSString stringWithFormat:@"Image responses: %@", imageResponses];
+    NSMutableString *imageRequestsString = [stringWithLengthEqualToReportWidthAndFilledWithSpaces mutableCopy];
+    [imageRequestsString replaceCharactersInRange:NSMakeRange(0, imageRequestsKey.length) withString:imageRequestsKey];
+    [imageRequestsString replaceCharactersInRange:NSMakeRange(REPORT_WIDTH - imageRequestsValue.length, imageRequestsValue.length) withString:imageRequestsValue];
+
+
+    NSString *imageResponsesKey = @"Responses:";
+    NSString *imageResponsesValue = [[data valueForKey:AFNetworkingMeterDataImageResponses] stringValue];
+
+    NSMutableString *imageResponsesString = [stringWithLengthEqualToReportWidthAndFilledWithSpaces mutableCopy];
+    [imageResponsesString replaceCharactersInRange:NSMakeRange(0, imageResponsesKey.length) withString:imageResponsesKey];
+    [imageResponsesString replaceCharactersInRange:NSMakeRange(REPORT_WIDTH - imageResponsesValue.length, imageResponsesValue.length) withString:imageResponsesValue];
 
     NSNumber *imageBytesReceived = [data valueForKey:AFNetworkingMeterDataImageBytesReceived];
     NSString *imageBytesReceivedString = [NSString stringWithFormat:@"Image data received (bytes): %@", imageBytesReceived];
@@ -100,24 +122,58 @@ NSString *NSStringFromCharacterAndLength(NSString *character, NSUInteger length)
     NSString *connectionErrorsString = [NSString stringWithFormat:@"Connection errors: %@", connectionErrors];
 
 #pragma mark Aggregation of the formatted report
+    NSString *headerTop = NSStringFromCharacterAndLength(@"=", REPORT_WIDTH);
+    NSString *headerTitle = @"   AFNetworkingMeter  -  formatted report   ";
+    NSString *headerBottom = NSStringFromCharacterAndLength(@"-", REPORT_WIDTH);
+    NSString *headerString = [@[headerTop, headerTitle, headerBottom] componentsJoinedByString:@"\n"];
 
     [formattedDataComponents addObject:@"\n"];
     [formattedDataComponents addObject:headerString];
+
+    [formattedDataComponents addObject:@"\n\n"];
+
     [formattedDataComponents addObject:requestsString];
+    [formattedDataComponents addObject:@"\n"];
     [formattedDataComponents addObject:responsesString];
+
+    [formattedDataComponents addObject:@"\n\n"];
+
     [formattedDataComponents addObject:bytesSentString];
+    [formattedDataComponents addObject:@"\n"];
+
     [formattedDataComponents addObject:bytesReceivedString];
+
+    [formattedDataComponents addObject:@"\n\n"];
+    [formattedDataComponents addObject:@"Elapsed time for request ..................."];
+    [formattedDataComponents addObject:@"\n\n"];
+
     [formattedDataComponents addObject:minimalElapsedTimeString];
+    [formattedDataComponents addObject:@"\n"];
+
     [formattedDataComponents addObject:maximalElapsedTimeString];
+
+    [formattedDataComponents addObject:@"\n\n"];
+    [formattedDataComponents addObject:@"Images ....................................."];
+    [formattedDataComponents addObject:@"\n\n"];
+
     [formattedDataComponents addObject:imageRequestsString];
+    [formattedDataComponents addObject:@"\n"];
+
     [formattedDataComponents addObject:imageResponsesString];
+    [formattedDataComponents addObject:@"\n"];
+
     [formattedDataComponents addObject:imageBytesReceivedString];
+
+    [formattedDataComponents addObject:@"\n\n"];
+    [formattedDataComponents addObject:@"Server errors .............................."];
+    [formattedDataComponents addObject:@"\n\n"];
+
     [formattedDataComponents addObject:totalServerErrorsString];
     [formattedDataComponents addObject:serverErrorsString];
     [formattedDataComponents addObject:totalConnectionErrorsString];
     [formattedDataComponents addObject:connectionErrorsString];
 
-    return [formattedDataComponents componentsJoinedByString:@"\n"];
+    return [formattedDataComponents componentsJoinedByString:@""];
 }
 
 @end
