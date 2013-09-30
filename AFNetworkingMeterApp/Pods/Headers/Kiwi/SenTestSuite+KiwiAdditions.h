0edf3ff27c92ec727e24bb73fbc86b218e368d15
stanislaw <s.pankevich@gmail.com>
2013-09-30 14:57:51 +0300

    Add .gitignore and AFNetworkingMeterApp/.gitignore

--- AFNetworkingMeterApp/Pods/Headers/Kiwi/SenTestSuite+KiwiAdditions.h
+++
@@ -1,1 +0,0 @@
-../../Kiwi/Classes/Core/SenTestSuite+KiwiAdditions.h
--- AFNetworkingMeterApp/Pods/Pods-AFNetworkingMeterAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AFNetworkingMeterAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_AFNetworkingMeterAppTests
-@end
